123a6aca6da331d6b050bd8448a4d367d56f4b26
Seeroy <37861929+Seeroy@users.noreply.github.com>
2025-02-25 17:37:13 +0300

    add: Отключение анимации загрузки + обновление EULA

---
+++ web/modals/eula.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    if(FrogConfig.read("eulaConfirmed", false) !== true){
+        $("#modal-eula").show();
+        $("#modal-eula .data").load("https://froglauncher.ru/privacy-policy");
+    }
+})
+
+// При подтверждении
+$("#modal-eula .confirm").click(() => {
+    $("#modal-eula").hide();
+    FrogConfig.write("eulaConfirmed", true);
+})
+
+// При отклонении
+$("#modal-eula .decline").click(() => {
+    FrogUI.closeMainWindow();
+})
